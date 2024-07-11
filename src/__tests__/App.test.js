2a5a2b06330e4362f0a47a818398631493134893
Jacob DiBiase <jakesback93@hotmail.com>
2024-07-10 19:26:19 -0700

    Initialize first unit test for App component

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,9 @@
+import { render } from "@testing-library/react";
+import App from "../App";
+
+describe("<App /> component", () => {
+  test("renders list of events", () => {
+    const AppDOM = render(<App />).container.firstChild;
+    expect(AppDOM.querySelector("#event-list")).toBeInTheDocument();
+  });
+});
