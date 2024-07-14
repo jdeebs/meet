6ff6a05cd6f8c4fe954259991bd56c191efb2cde
Jacob DiBiase <jakesback93@hotmail.com>
2024-07-14 01:44:16 -0700

    Add 'renders event title correctly' test to Event.test.js

--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -1,15 +1,23 @@
 import { render } from "@testing-library/react";
 import Event from "../components/Event";
-import mockData from "../mock-data";
+import { getEvents } from "../api";
 
 describe("<Event /> component", () => {
-    test("renders event title correctly", () => {
-        // Select first event from the mock data
-        const mockEvent = mockData[0];
-        // Render Event component with the mock event
-        const { queryByText } = render(<Event event={mockEvent} />);
-
-        // Check if the summary(title) is rendered
-        expect(queryByText(mockEvent.summary)).toBeInTheDocument();
-    })
+  // Before each test, render the Event component and store the result
+  let EventComponent;
+  let event;
+  beforeEach(async () => {
+    // Fetch events using getEvents function
+    const allEvents = await getEvents();
+    // Use the first event from fetched data
+    event = allEvents[0];
+    // Render Event component with the first mock event
+    EventComponent = render(<Event event={event} />);
+    
+  });
+  test("renders event title correctly", () => {
+    const { queryByText } = EventComponent;
+    // Check if the summary(title) is rendered
+    expect(queryByText(event.summary)).toBeInTheDocument();
+  });
 });
--- src/components/Event.js
+++ src/components/Event.js
@@ -2,7 +2,7 @@ const Event = ({ event }) => {
   return (
     <ul id="event">
       <h2>{event.summary}</h2>
-      <p>{event.start.dateTime}</p>
+      <p>{event.created}</p>
       <p>{event.location}</p>
       <button>Show Details</button>
     </ul>
