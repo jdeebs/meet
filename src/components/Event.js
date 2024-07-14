b6e8b75ec32954d23abe42ce14bbcff9de9692c5
Jacob DiBiase <jakesback93@hotmail.com>
2024-07-14 00:17:23 -0700

    Initialize Event Component

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,5 +1,12 @@
-const Event = () => {
-  return <li></li>;
+const Event = ({ event }) => {
+  return (
+    <div id="event">
+      <h2>{event.summary}</h2>
+      <p>{event.start.dateTime}</p>
+      <p>{event.location}</p>
+      <button>Show Details</button>
+    </div>
+  );
 };
 
 export default Event;
