b676db5c47304b55c5df4178d7e9b7745f30b6a5
Jacob DiBiase <jakesback93@hotmail.com>
2024-07-14 12:13:10 -0700

    Update App.js to include NumberOfEvents component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
-import CitySearch from './components/CitySearch';
+import CitySearch from "./components/CitySearch";
 import EventList from "./components/EventList";
+import NumberOfEvents from "./components/NumberOfEvents";
 import "./App.css";
 
 function App() {
+  const [numberOfEvents, setNumberOfEvents] = useState(32);
+
   return (
     <div className="App">
       <CitySearch />
+      <NumberOfEvents setNumberOfEvents={setNumberOfEvents} />
       <EventList />
     </div>
   );
