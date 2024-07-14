f00332e794ec517a6df6a85d59518e1b8a18a847
Jacob DiBiase <jakesback93@hotmail.com>
2024-07-14 00:11:24 -0700

    Initialize Event.test.js

---
+++ src/__tests__/Event.test.js
@@ -0,0 +1,8 @@
+import { render } from "@testing-library/react";
+import Event from "../components/Event";
+import mockData from "../mock-data";
+
+describe("<Event /> component", () => {
+
+    
+});
