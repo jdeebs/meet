289839bdf0cdb9acba656c3c356c3dafedc2b50f
Jacob DiBiase <jakesback93@hotmail.com>
2024-07-15 10:02:28 -0700

    Update App.js to include correct props and state updates from components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import CitySearch from "./components/CitySearch";
 import EventList from "./components/EventList";
 import NumberOfEvents from "./components/NumberOfEvents";
+import { getEvents, extractLocations } from "./api";
 import "./App.css";
 
 function App() {
-  const [setNumberOfEvents] = useState(32);
+  const [events, setEvents] = useState([]);
+  const [numberOfEvents, setNumberOfEvents] = useState(32);
+  const [locations, setLocations] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const events = await getEvents();
+      setEvents(events.slice(0, numberOfEvents));
+      setLocations(extractLocations(events));
+    };
+    fetchData();
+  }, [numberOfEvents]);
 
   return (
     <div className="App">
-      <CitySearch />
+      <h1 id="title">Meet App</h1>
+      <p id="subheading">Choose your nearest city</p>
+      <CitySearch allLocations={locations} />
       <NumberOfEvents setNumberOfEvents={setNumberOfEvents} />
-      <EventList />
+      <EventList events={events} />
     </div>
   );
 }
