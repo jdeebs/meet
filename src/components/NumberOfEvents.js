1f4cd5b98e590ae21708829870220a774583965e
Jacob DiBiase <jakesback93@hotmail.com>
2024-07-16 18:01:28 -0700

    Update NumberOfEvents.js input to be more user friendly and less error prone

--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -5,16 +5,17 @@ const NumberOfEvents = ({ setNumberOfEvents }) => {
 
   const handleInputChange = (event) => {
     const value = event.target.value;
-    // Check if the input is empty or not a number
-    if (value === "" || isNaN(parseInt(value, 10))) {
-      // If empty or NaN, set number to 32
-      setNumber(0);
-      setNumberOfEvents(0);
-    } else {
-      // Otherwise, parse the input value
-      const parsedValue = parseInt(value, 10);
-      setNumber(parsedValue);
-      setNumberOfEvents(parsedValue);
+    setNumber(value);
+
+    // If user deletes input field, set number of events to 10 and leave input field blank
+    if (value === "") {
+      setNumber("");
+      setNumberOfEvents(10);
+    }
+
+    // Validate and set number of events if the input is a valid number
+    if (!isNaN(value) && value !== "") {
+      setNumberOfEvents(parseInt(value, 10));
     }
   };
 
@@ -26,6 +27,9 @@ const NumberOfEvents = ({ setNumberOfEvents }) => {
         id="number-of-events"
         value={number}
         onChange={handleInputChange}
+        placeholder="32"
+        min="0"
+        max="32"
         data-testid="number-input"
       />
     </div>
