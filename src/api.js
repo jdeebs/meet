8530a81f59b46fe9f366f15db0429bbf131268c3
Jacob DiBiase <jakesback93@hotmail.com>
2024-07-25 23:37:47 -0700

    Add error handling to checkToken function

--- src/api.js
+++ src/api.js
@@ -21,6 +21,11 @@ const checkToken = async (accessToken) => {
   const response = await fetch(
     `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
   );
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
   const result = await response.json();
   return result;
 };
