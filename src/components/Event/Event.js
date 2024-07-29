c69f817c2c004a90171c2547b88a9ac50b24bfd9
Jacob DiBiase <jakesback93@hotmail.com>
2024-07-29 11:54:45 -0700

    Refactor Event.js to show event details, format dates

--- src/components/Event/Event.js
+++ src/components/Event/Event.js
@@ -5,20 +5,22 @@ const Event = ({ event }) => {
   // State to track if the event details are shown
   const [showDetails, setShowDetails] = useState(false);
 
-  // Function to toggle the visibility of event details
-  const handleToggleDetails = () => {
-    setShowDetails((prevShowDetails) => !prevShowDetails);
-  };
-
   return (
     <ul id="event">
-      <h2>{event.summary}</h2>
-      <p>{event.created}</p>
-      <p>{event.location}</p>
-      <button id="show-details" onClick={handleToggleDetails}>
+      <h2>{event && event.summary}</h2>
+      <p>{event && event.location}</p>
+      <p>{event && new Date(event.created).toUTCString()}</p>
+      {showDetails ? (
+        <p className="details">{event && event.description}</p>
+      ) : null}
+      <button
+        id="show-details"
+        onClick={() => {
+          showDetails ? setShowDetails(false) : setShowDetails(true);
+        }}
+      >
         {showDetails ? "Hide Details" : "Show Details"}
       </button>
-      {showDetails && <p>Event Details</p>}
     </ul>
   );
 };
