623d2214964bff8abf354eb31383d3b6a3cd43e1
Jacob DiBiase <jakesback93@hotmail.com>
2024-08-01 00:13:52 -0700

    Add scenario 2 test to EndToEnd.test.js

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -13,10 +13,25 @@ describe("show/hide an event details", () => {
     // Ensure event list is loaded (event selector) before moving on
     await page.waitForSelector("#event");
 
-    // Check if event details isn't null (not shown to user)
+    // Check if event details is null (not shown to user)
     const eventDetails = await page.$("#event .details");
     expect(eventDetails).toBeNull();
     // Close Chromium window
+    browser.close();
+  });
+
+  test("User can expand an event to see its details", async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto("http://localhost:3000/");
+
+    await page.waitForSelector("#event");
+    // Click on show details button
+    await page.click("#event #show-details");
+
+    // Check if event details is defined (shown to user)
+    const eventDetails = await page.$("#event #show-details");
+    expect(eventDetails).toBeDefined();
     browser.close();
   });
 });
