b0a73e7df4228dfd798a1c0b2763ff1c489c3d49
Jacob DiBiase <jakesback93@hotmail.com>
2024-08-08 12:33:19 -0700

    Initialize Alert class component, integrate into App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import CitySearch from "./components/CitySearch/CitySearch";
 import EventList from "./components/EventList/EventList";
 import NumberOfEvents from "./components/NumberOfEvents/NumberOfEvents";
+import { InfoAlert } from "./components/Alert/Alert";
 import { getEvents } from "./api";
 import "./App.css";
 
@@ -11,6 +12,7 @@ const App = () => {
   const [numberOfEvents, setNumberOfEvents] = useState(32);
   // State for selected city to filter events
   const [selectedCity, setSelectedCity] = useState("");
+  const [infoAlert, setInfoAlert] = useState("");
 
   // Hook to fetch data whenever numberOfEvents or selectedCity changes
   useEffect(() => {
@@ -28,8 +30,14 @@ const App = () => {
 
   return (
     <div className="App">
+      <div className="alerts-container">
+        {infoAlert.length ? <InfoAlert text={infoAlert} /> : null}
+      </div>
       <h1 id="title">Meet App</h1>
-      <CitySearch onCitySelect={handleCitySelection} />
+      <CitySearch
+        onCitySelect={handleCitySelection}
+        setInfoAlert={setInfoAlert}
+      />
       <NumberOfEvents setNumberOfEvents={setNumberOfEvents} />
       <EventList events={events} />
     </div>
