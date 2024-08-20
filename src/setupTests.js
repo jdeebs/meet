de6e154511e6e933dc92cabcb6cf2e75c5da4e27
Jacob DiBiase <jakesback93@hotmail.com>
2024-08-20 13:30:11 -0700

    Fix ResizeObserver related test errors

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,21 +2,40 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
 
 // Here, add portions of the warning messages you want to intentionally prevent from appearing
 const MESSAGES_TO_IGNORE = [
-    "When testing, code that causes React state updates should be wrapped into act(...):",
-    "Error:",
-    "The above error occurred",
-    "Warning: `ReactDOMTestUtils.act` is deprecated in favor of `React.act`. Import `act` from `react` instead of `react-dom/test-utils`. See https://react.dev/warnings/react-dom-test-utils for more info."
-  ];
-  
-  const originalError = console.error.bind(console.error);
-  
-  console.error = (...args) => {
-    const ignoreMessage = MESSAGES_TO_IGNORE.find(message => args.toString().includes(message));
-    if (!ignoreMessage) originalError(...args);
-  }
+  "When testing, code that causes React state updates should be wrapped into act(...):",
+  "Error:",
+  "The above error occurred",
+  "Warning: `ReactDOMTestUtils.act` is deprecated in favor of `React.act`. Import `act` from `react` instead of `react-dom/test-utils`. See https://react.dev/warnings/react-dom-test-utils for more info.",
+];
 
-  jest.setTimeout(30000);
+const originalError = console.error.bind(console.error);
+
+console.error = (...args) => {
+  const ignoreMessage = MESSAGES_TO_IGNORE.find((message) =>
+    args.toString().includes(message)
+  );
+  if (!ignoreMessage) originalError(...args);
+};
+
+jest.setTimeout(30000);
+
+const { ResizeObserver } = window;
+
+beforeEach(() => {
+  //@ts-ignore
+  delete window.ResizeObserver;
+  window.ResizeObserver = jest.fn().mockImplementation(() => ({
+    observe: jest.fn(),
+    unobserve: jest.fn(),
+    disconnect: jest.fn(),
+  }));
+});
+
+afterEach(() => {
+  window.ResizeObserver = ResizeObserver;
+  jest.restoreAllMocks();
+});
